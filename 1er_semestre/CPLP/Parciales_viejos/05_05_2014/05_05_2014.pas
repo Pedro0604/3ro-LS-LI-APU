25e017d686708c299b98fc19fb691ed639769384
Pedro0604 <pedrospadari0604@gmail.com>
2024-04-25 15:41:05 -0300

    CPLP - Parcial 05_05_2014

---
+++ 1er_semestre/CPLP/Parciales_viejos/05_05_2014/05_05_2014.pas
@@ -0,0 +1,52 @@
+Program parcial;
+    Type 
+        vector = array [1..10] Of integer;
+
+    Var 
+        p, q:   integer;
+        pint:   integer;
+
+
+    Procedure auxiliar();
+        Var 
+            aux, i, pint:   integer;
+            v:   vector;
+        Begin
+            pint := 5;
+            For i:=1 To 10 Do
+                v[i] := p * i;
+            aux := p + 1;
+            p := q + 2;
+            v[p - 3] := v[pint] - 4;
+            If v[aux] = 30 Then
+                // valor es llamada a función (╯°□°）╯︵ ┻━┻
+                v[aux] := v[valor] + 5;
+        End;
+
+
+    Function valor():   integer;
+        Var 
+            h:   integer;
+        Function q():   integer;
+            Begin
+                return (pint - 1);
+            End;
+
+        Begin //Function valor
+            // q es llamada a función (╯°□°）╯︵ ┻━┻
+            h := 2 + q;
+            return ((p + h) - 9);
+        End;
+
+
+    Begin // Program
+        p := 5;
+        q := 10;
+        pint := 3;
+        auxiliar();
+
+        // valor es llamada a función (╯°□°）╯︵ ┻━┻
+        q := valor + 9;
+        writeln('q: ', q);
+        writeln('p: ', p);
+    End.
